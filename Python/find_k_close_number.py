c7ec2d0c09bce3e70579518dd446329bb97b3132
Runze <runze@twobulls.com>
2018-10-11 14:01:58 -0400

    Add another binary search method for find k close elements from arr.

--- Python/find_k_close_number.py
+++ Python/find_k_close_number.py
@@ -9,13 +9,23 @@ def find_k_close_nums_op1(k, n, nums): # it's linear O(len(nums) - k)
     return nums[left:right + 1]
 
 def find_k_close_nums_op2(k, n, nums): # it's binary search O(log(len(nums)))
-    pass
+    left = 0
+    right = len(nums) - k
+    while left < right:
+        mid = (left + right) // 2
+        if n - nums[mid] > nums[mid + k] - n:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left: left + k]
 
 def main():
-    num = 100
-    k = 2
+    num = 34
+    k = 5
     nums = [1, 3, 5, 12, 23, 24, 26, 33, 39, 45, 55, 67, 80, 81, 83]
-    result = find_k_close_nums_op1(k, num, nums)
-    print(result)
+    result1 = find_k_close_nums_op1(k, num, nums)
+    result2 = find_k_close_nums_op2(k, num, nums)
+    print(result1)
+    print(result2)
 
 main()
